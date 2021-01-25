5f98afff04bbbc7e887f2d5b649b198b38cfdd26
Chocological <cytusomega@gmail.com>
2021-01-24 18:25:56 -0800

    bugs fixed and added functions

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Functional conway's game of life. Prints results in console for now, will add graphics later.
 
 Graphics added using pygame, but found bugs in flip logic
+
+Flip logic fixed, graphics perfected, can now start/pause by pressing any key and add grid by clicking with mouse
--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import random as r
 
 pygame.init()
 pygame.display.set_caption("Conway's game of life")
-screen = pygame.display.set_mode((600, 600))    
+screen = pygame.display.set_mode((700, 700))    
 screen.fill((250, 200, 200))
 clock = pygame.time.Clock()
 
@@ -22,24 +22,24 @@ def flip(x, y):
                     elif life[a][b] == 0:
                         life[a][b] = 1
     return
+'''
+startcount = 1000
+for q in range(startcount):
+    flip(r.randint(0, gridsize), r.randint(0, gridsize))
 
-#startcount = 200
-#for q in range(startcount):
-#    flip(r.randint(0, gridsize), r.randint(0, gridsize))
-
-flip(1, 2)
-flip(2, 1)
-flip(3, 1)
-flip(2, 3)
-flip(4, 2)
-flip(5, 3)
-flip(5, 4)
-flip(4, 4)
-flip(3, 4)
-
+flip(51, 52)
+flip(52, 51)
+flip(53, 51)
+flip(54, 51)
+flip(55, 52)
+flip(51, 53)
+flip(52, 53)
+flip(54, 53)
+flip(55, 53)
+'''
 def check(x, y):
     lifecount = 0
-    leftx = x - 1
+    leftx = x - 1 
     rightx = x + 1
     if rightx >= gridsize:
         rightx -= gridsize
@@ -47,53 +47,61 @@ def check(x, y):
     downy = y + 1
     if downy >= gridsize:
         downy -= gridsize  
-    if life[leftx][upy]:
+    if life[upy][leftx]:
         lifecount += 1
-    if life[leftx][y]:
+    if life[y][leftx]:
         lifecount += 1
-    if life[leftx][downy]:
+    if life[downy][leftx]:
         lifecount += 1
-    if life[x][upy]:
+    if life[upy][x]:
         lifecount += 1
-    if life[x][downy]:
+    if life[downy][x]:
         lifecount += 1   
-    if life[rightx][upy]:
+    if life[upy][rightx]:
         lifecount += 1
-    if life[rightx][y]:
+    if life[y][rightx]:
         lifecount += 1
-    if life[rightx][downy]:
+    if life[downy][rightx]:
         lifecount += 1     
     return lifecount
 
+def flipList(life):
+    to_flip = []
+    for a in range(len(life)):
+        for b in range(len(life[a])):
+            nearby = check(b, a)
+            if life[a][b]:
+                if nearby <= 1:
+                    to_flip.append([b, a])
+                if nearby >= 4:
+                    to_flip.append([b, a])
+            else:
+                if nearby == 3:
+                    to_flip.append([b, a])
+    return to_flip                
+
 state = False
 while True:
+    screen.fill((250, 200, 200))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
             state = not state
-    to_flip = []
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mousex, mousey = pygame.mouse.get_pos()
+            flip(mousex//sqsize, mousey//sqsize)
     if state == True:        
-        for a in range(len(life)):
-            for b in range(len(life[a])):
-                nearby = check(a, b)
-                if life[a][b]:
-                    if nearby <= 1:
-                        to_flip.append([a, b])
-                    if nearby >= 4:
-                        to_flip.append([a, b])
-                else:
-                    if nearby == 3:
-                        to_flip.append([a, b])
+        to_flip = flipList(life)
         for x, y in to_flip:
             flip(x, y)
     for i in range(len(life)):
         for j in range(len(life[i])):
             if life[i][j]:
-                pygame.draw.rect(screen, (20,50,200), (i*sqsize, j*sqsize, sqsize, sqsize))
+                pygame.draw.rect(screen, (20,50,200), (j*sqsize, i*sqsize, sqsize, sqsize))
     pygame.display.update()
-    screen.fill((250, 200, 200))
     msElapsed = clock.tick(3)
 
                 
